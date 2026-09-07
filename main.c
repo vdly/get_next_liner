d9d059c215792f3e3ad20f74415c6fc20cad1aff
Jodene Hii <jodehii@u80z04s06.42kl.edu.my>
2026-09-07 14:48:32 +0800

    maini

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jodehii <jodehii@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/06 21:55:13 by jodehii           #+#    #+#             */
-/*   Updated: 2026/09/06 22:23:31 by jodehii          ###   ########.fr       */
+/*   Updated: 2026/09/07 14:48:03 by jodehii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,15 @@
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
+	char	*ptr;
 
+	ptr = get_next_line(fd);
 	fd = open("text.txt", O_RDWR);
-	char *ptr = get_next_line(fd);
 	while (ptr != NULL)
 	{
 		printf("%s", ptr);
 		free(ptr);
 		ptr = get_next_line(fd);
 	}
-	// printf("%s", get_next_line(fd));
-	// printf("%s", get_next_line(fd));
-	// printf("%s", get_next_line(fd));
-	// printf("%s", get_next_line(fd));
-	return(0);
 }
